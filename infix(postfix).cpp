6596dc454d2bd464718ac1c58c4301ab1030c087
WesleyGoo <99233770+WesleyGoo@users.noreply.github.com>
2022-04-10 22:10:36 -0700

    Add files via upload

--- infix(postfix).cpp
+++ infix(postfix).cpp
@@ -8,6 +8,14 @@ using namespace std;
 
 //This entire code is an expression validation
 
+bool valObject(char c)
+{
+	if (c >= '0' && c <= '9') return true;
+	if (c >= 'a' && c <= 'z') return true;
+	if (c >= 'A' && c <= 'Z') return true;
+	return false;
+}
+
 bool valDigit(char c)
 {
 	return (c >= '0' && c <= '9');
@@ -40,7 +48,7 @@ bool evaluate(string s)
 	int parCount = 0;
 	int valCount = 0;
 	int negCount = 0;
-	bool compeleteVal = false;
+	bool completeVal = false;
 	bool validExpress = true;
 	while (index < s.length())
 	{
@@ -49,35 +57,43 @@ bool evaluate(string s)
 			break;
 		}
 		char position = s[index];
-		if (valDigit(position) == false && valOperator(position) == false)
+		if (valObject(position) == false && valOperator(position) == false && position != ' ')
 		{
 			cout << "Invalid expression due to use of illegal elements, please create a valid expression without spaces or characters." << endl;
 			validExpress = false;
 		}
-		if (valDigit(position))
+		if (position == ' ')
 		{
-			compeleteVal = false;
-			val = (val * 10) + (int)(position - '0');
+			index++;
+			position = s[index];
+		}
+		if (valObject(position))
+		{
+			completeVal = false;
+			if (valDigit(position))
+			{
+				val = (val * 10) + (int)(position - '0');
+			}
 		}
 		else if (valOperator(position))
 		{
 			if (completeVal == false)
 			{
 				valCount = valCount++;
-				compeleteVal = true;
+				completeVal = true;
 				negCount = 0;
 			}
 			if (position == '-')
 			{
-				//checks for double negative
-				if (negCount != 0)
-				{
-					cout << "Invalid expression due to excess use of operators before translation to prefix." << endl;
-					validExpress = false;
-				}
+				//checks for use without a value preceding operator
 				if (valCount == 0)
 				{
-					negCount = negCount++;
+					cout << "Invalid expression due to excess use of operators." << endl;
+					validExpress = false;
+				}
+				else
+				{
+					valCount = valCount--;
 					val = 0;
 				}
 			}
@@ -86,7 +102,7 @@ bool evaluate(string s)
 				//checks for use without a value preceding operator
 				if (valCount == 0)
 				{
-					cout << "Invalid expression due to excess use of operators before translation to prefix." << endl;
+					cout << "Invalid expression due to excess use of operators." << endl;
 					validExpress = false;
 				}
 				else
@@ -100,9 +116,8 @@ bool evaluate(string s)
 				//checks for use without a value preceding operator
 				if (valCount == 0)
 				{
-					cout << "Invalid expression due to excess use of operators before translation to prefix." << endl;
+					cout << "Invalid expression due to excess use of operators." << endl;
 					validExpress = false;
-					break;
 				}
 				else
 				{
@@ -115,9 +130,22 @@ bool evaluate(string s)
 				//checks for use without a value preceding operator
 				if (valCount == 0)
 				{
-					cout << "Invalid expression due to excess use of operators before translation to prefix." << endl;
+					cout << "Invalid expression due to excess use of operators." << endl;
 					validExpress = false;
-					break;
+				}
+				else
+				{
+					valCount = valCount--;
+					val = 0;
+				}
+			}
+			if (position == '^')
+			{
+				//checks for use without a value preceding operator
+				if (valCount == 0)
+				{
+					cout << "Invalid expression due to excess use of operators." << endl;
+					validExpress = false;
 				}
 				else
 				{
@@ -137,7 +165,6 @@ bool evaluate(string s)
 				{
 					cout << "Invalid expression due to ')', please ensure the use of parantheses is legal." << endl;
 					validExpress = false;
-					break;
 				}
 				if (parCount != 0)
 				{
@@ -150,12 +177,13 @@ bool evaluate(string s)
 	}
 	if (validExpress == false)
 	{
-		break;
+		return false;
 	}
 	//unequal parantheses check after reading string
 	if (parCount != 0)
 	{
+		cout << parCount << endl;
 		cout << "Invalid expression due to excess use of parantheses, please create a valid expression" << endl;
-		break;
+		return false;
 	}
 }
